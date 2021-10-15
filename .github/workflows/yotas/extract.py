e6073795c1ce7da25f26c847e9733bc6a9083bb2
hawawout2000@gmail.com <hawawout2000@gmail.com>
2021-10-15 12:44:21 +0100

    retry

--- .github/workflows/yotas/extract.py
+++ .github/workflows/yotas/extract.py
@@ -1,6 +1,6 @@
 import urllib.request
 import json
-# import pandas
+import pandas
 
 url = "https://miniyotas.osscameroon.com/v1/api/records"
 response = urllib.request.urlopen( url )
@@ -9,6 +9,7 @@ data_json = json.loads( response.read() )
 
 for item in data_json:
     print (item['github_handle'], item['yotas'])
+    
 
  
 
