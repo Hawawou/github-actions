d78818fc955646b3f58cde1e82d04b915c651691
hawawout2000@gmail.com <hawawout2000@gmail.com>
2021-10-15 12:28:54 +0100

    members and yotas

--- .github/workflows/yotas/extract.py
+++ .github/workflows/yotas/extract.py
@@ -1,2 +1,17 @@
-with open('file.txt', 'r') as reader:
-    print(reader.read()) 
+import urllib.request
+import json
+# import pandas
+
+url = "https://miniyotas.osscameroon.com/v1/api/records"
+response = urllib.request.urlopen( url )
+
+data_json = json.loads( response.read() )
+
+for item in data_json:
+    print (item['github_handle'], item['yotas'])
+
+ 
+
+    
+    
+
