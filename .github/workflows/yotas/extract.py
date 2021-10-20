13b3baa0e1829bce544454d0492fa9dba9ec24b0
hawawout2000@gmail.com <hawawout2000@gmail.com>
2021-10-20 21:52:45 +0100

    remove pandas

--- .github/workflows/yotas/extract.py
+++ .github/workflows/yotas/extract.py
@@ -1,6 +1,5 @@
 import urllib.request
 import json
-import pandas as pd
 
 url = "https://miniyotas.osscameroon.com/v1/api/records"
 response = urllib.request.urlopen( url )
@@ -11,16 +10,11 @@ message = " Number of yotas each member has"
 print(message)
 print("```")
 
-
 data_json.sort(key = lambda item: int(item['yotas']), reverse = True)
 for item in data_json:
     if int(item['yotas']) > 0:
         print(item['github_handle'], item['yotas'])
-    
 
-
-     
-    
 print("```")
     
     
