f25afb740ed57feb33253b86f79baf42e3984d14
hawawout2000@gmail.com <hawawout2000@gmail.com>
2021-10-20 16:54:56 +0100

    send to telegram

--- .github/workflows/yotas/extract.py
+++ .github/workflows/yotas/extract.py
@@ -5,13 +5,20 @@ url = "https://miniyotas.osscameroon.com/v1/api/records"
 response = urllib.request.urlopen( url )
 
 data_json = json.loads( response.read() )
+message = " Number of yotas each member has"
+
+print(message)
 
 for item in data_json:
-    print (item['github_handle'], item['yotas'])
+    print(item['github_handle'], item['yotas'])
+    
+    
+    
+    
     
 
  
 
     
-    
+
 
