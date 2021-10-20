86e95dd557bafebee4158543b8151fcdfe3be678
hawawout2000@gmail.com <hawawout2000@gmail.com>
2021-10-20 17:40:02 +0100

    add bot

--- .github/workflows/yotas/extract.py
+++ .github/workflows/yotas/extract.py
@@ -8,9 +8,11 @@ data_json = json.loads( response.read() )
 message = " Number of yotas each member has"
 
 print(message)
+print("```")
 
 for item in data_json:
-    print(item['github_handle'], item['yotas'])
+    print(item['github_handle':], item['yotas'])
+    print("```")
     
     
     
