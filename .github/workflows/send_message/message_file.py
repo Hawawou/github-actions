643ae4768b15adb1fbbd7bff634c4c31b978c428
Hawawou <hawawout2000@gmail.com>
2021-12-09 19:14:05 +0100

    read messages from yaml file

--- .github/workflows/send_message/message_file.py
+++ .github/workflows/send_message/message_file.py
@@ -1,5 +1,17 @@
-import random 
-messages = ["Hello you welcome", "Thank you for joining us", "Feel free"]
+import random
+import yaml
+
+with open('message_file.yaml', 'r') as f:
+    messages = yaml.safe_load(f)
+    
 
 
-print(random.choice(messages))
+for i in messages:
+    message1 = i['message_name_1']['message']
+    message2 = i['message_name_2']['message']
+    message3 = i['message_name_3']['message']
+    message4 = i['message_name_4']['message']
+
+    message_list = random.choice[message1, message2, message3, message4]
+    print(message_list)
+    # print(message1)
