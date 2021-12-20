536801be8d3ffafb9da0bb0ecfd99625ab43226e
Hawawou <hawawout2000@gmail.com>
2021-12-20 06:38:17 +0100

    get messages from yaml file

--- .github/workflows/send_message/message_file.py
+++ .github/workflows/send_message/message_file.py
@@ -4,14 +4,9 @@ import yaml
 with open('message_file.yaml', 'r') as f:
     messages = yaml.safe_load(f)
     
+message_block_chosed = random.choice(messages)
+print(message_block_chosed['message_name']['message'])
+   
+    
 
 
-for i in messages:
-    message1 = i['message_name_1']['message']
-    message2 = i['message_name_2']['message']
-    message3 = i['message_name_3']['message']
-    message4 = i['message_name_4']['message']
-
-    message_list = random.choice[message1, message2, message3, message4]
-    print(message_list)
-    # print(message1)
